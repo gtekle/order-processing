473c9b7ad01ab43f9b9890171dbaf22638119766
tYohannes <teklegy@gmail.com>
2022-05-02 17:25:16 +0300

    Refactor code in routes.rb to change sequence of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "customers#index"
-
   resources :products
   resources :customers
   resources :orders
+  
+  # Defines the root path route ("/")
+  root "customers#index"
 end
