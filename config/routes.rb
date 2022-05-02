5f34e0b8915dfd6d969b19f6e8d1a1c35bfddb9c
tYohannes <teklegy@gmail.com>
2022-05-02 08:50:59 +0300

    Generate Orders controller with index, show, new, create, edit, update and destroy actionsand corresponding views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'orders/index'
+  get 'orders/show'
+  get 'orders/new'
+  get 'orders/create'
+  get 'orders/edit'
+  get 'orders/update'
+  get 'orders/destroy'
   get 'customers/index'
   get 'customers/show'
   get 'customers/new'
