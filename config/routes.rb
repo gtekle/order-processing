401efd555b422e9c6871d7cda91e136fed179681
tYohannes <teklegy@gmail.com>
2022-05-02 09:00:30 +0300

    Update routes.rb to use 'resources' command for route to action mapping

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,11 @@
 Rails.application.routes.draw do
-  get 'orders/index'
-  get 'orders/show'
-  get 'orders/new'
-  get 'orders/create'
-  get 'orders/edit'
-  get 'orders/update'
-  get 'orders/destroy'
-  get 'customers/index'
-  get 'customers/show'
-  get 'customers/new'
-  get 'customers/create'
-  get 'customers/edit'
-  get 'customers/update'
-  get 'customers/destroy'
-  get 'products/index'
-  get 'products/show'
-  get 'products/new'
-  get 'products/create'
-  get 'products/edit'
-  get 'products/update'
-  get 'products/destroy'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :products
+  resources :customers
+  resources :orders
 end
