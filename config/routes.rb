3aee724ab84e9d133b0c0bb720ea1886aa7595c1
tYohannes <teklegy@gmail.com>
2022-05-02 08:41:02 +0300

    Generate Products controller with index, show, new, create, edit, update and destroy actionsand corresponding views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'products/index'
+  get 'products/show'
+  get 'products/new'
+  get 'products/create'
+  get 'products/edit'
+  get 'products/update'
+  get 'products/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
