e7bb1b9f29fecc7554a00df706c76c457914ce86
tYohannes <teklegy@gmail.com>
2022-05-02 21:51:19 +0300

    Refactor customer action methods to interact with database

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,15 +1,50 @@
 class CustomersController < ApplicationController
-  def index; end
+  def index
+    @customers = Customer.all
+  end
 
-  def show; end
+  def show
+    @customer = Customer.find(params[:id])
+  end
 
-  def new; end
+  def new
+    @customer = Customer.new
+  end
 
-  def create; end
+  def create
+    @customer = Customer.new(customer_params)
 
-  def edit; end
+    if @customer.save
+      redirect_to @customer
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
-  def update; end
+  def edit
+    @customer = Customer.find(params[:id])
+  end
 
-  def destroy; end
+  def update
+    @customer = Customer.find(params[:id])
+
+    if @customer.update(customer_params)
+      redirect_to @customer
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @customer = Customer.find(params[:id])
+    @customer.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :email, :phone_number)
+  end
 end
